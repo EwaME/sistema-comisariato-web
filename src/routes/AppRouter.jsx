05643d16d9be439d2f767c26498557128c0c8ed6
EwaME <edwardmaradiagaespinal@gmail.com>
2026-04-01 00:03:42 -0600

    AppRouter Unido

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -15,8 +15,6 @@ import Empleados from "../pages/Manage/Empleados/Gest_Empleados";
 import CrearEmpleado from "../pages/Manage/Empleados/CrearEmpleado";
 import DetalleEmpleado from "../pages/Manage/Empleados/DetalleEmpleado";
 
-import Inventario from "../pages/Manage/Productos/Gest_Inventario";
-import Categorias from "../pages/Manage/Productos/Gest_Categorias";
 import Gest_Roles from '../pages/Manage/Roles/Gest_Roles';
 import CrearRol from '../pages/Manage/Roles/CrearRol';
 
@@ -27,7 +25,7 @@ import Gest_Departamentos from '../pages/Manage/Departamentos/Gest_Departamentos
 import CrearDepartamento from '../pages/Manage/Departamentos/CrearDepartamento';
 
 import Inventario from '../pages/Manage/Inventario/Gest_Inventario';
-import CrearProducto from '../pages/Manage/Inventario/CrearProducto';
+import CrearProducto from '../pages/Manage/Inventario/CrearProducto.jsx';
 import Categorias from '../pages/Manage/Inventario/Gest_Categorias';
 
 import Sugerencias from "../pages/Manage/Gest_Sugerencias";
@@ -44,72 +42,6 @@ import Comentarios from "../pages/Manage/Gest_Comentarios";
 import GuiasyAyudas from "../pages/GuiasyAyudas";
 
 export default function AppRouter() {
-  return (
-    <Routes>
-      {/* --- RUTA PÚBLICA --- */}
-      <Route path="/login" element={<Login />} />
-      <Route path="/recuperar-password" element={<RecuperarPassword />} />
-      <Route path="/nueva-contrasena" element={<NuevaPassword />} />
-
-      {/* --- RUTAS PROTEGIDAS (Requieren inicio de sesión) --- */}
-      <Route
-        path="/"
-        element={
-          <ProtectedRoute>
-            <AdminLayout />
-          </ProtectedRoute>
-        }
-      >
-        {/* Redirección por defecto al entrar a la raíz "/" */}
-        <Route index element={<Navigate to="/dashboard" replace />} />
-
-        {/* 1. General */}
-        <Route path="dashboard" element={<Dashboard />} />
-
-        {/* 2. Administración */}
-        <Route path="usuarios" element={<Usuarios />} />
-        <Route path="usuarios/nuevo" element={<CrearUsuario />} />
-
-        <Route path="empleados" element={<Empleados />} />
-        <Route path="empleados/nuevo" element={<CrearEmpleado />} />
-        <Route path="empleados/editar/:id" element={<CrearEmpleado />} />
-        <Route path="empleados/detalle/:id" element={<DetalleEmpleado />} />
-        <Route path="configuraciones" element={<Configuraciones />} />
-
-        {/* 3. Gestión de Stock */}
-        <Route path="inventario" element={<Inventario />} />
-        <Route path="categorias" element={<Categorias />} />
-
-        {/* 4. Acreditaciones */}
-        <Route path="creditos" element={<Creditos />} />
-        <Route path="creditos/revision/:id" element={<RevisionCredito />} />
-        <Route path="creditos/detalle/:id" element={<DetalleCredito />} />
-        <Route path="reclamos" element={<Reclamos />} />
-
-        {/* 5. Análisis */}
-        <Route path="reportes" element={<Reportes />} />
-
-        {/* 6. Comunidad */}
-        <Route path="sugerencias" element={<Sugerencias />} />
-        <Route path="comentarios" element={<Comentarios />} />
-        <Route path="guias" element={<GuiasyAyudas />} />
-      </Route>
-
-      {/* --- RUTA 404 (Si el usuario escribe una URL que no existe) --- */}
-      <Route
-        path="*"
-        element={
-          <div className="min-h-screen flex flex-col items-center justify-center bg-[#F8FAFC]">
-            <h1 className="text-6xl font-extrabold text-[#020817] mb-4">404</h1>
-            <p className="text-gray-500 font-medium">
-              Página no encontrada o en construcción 🚧
-            </p>
-          </div>
-        }
-      />
-    </Routes>
-  );
-}
     return (
         <Routes>
             {/* --- RUTA PÚBLICA --- */}
@@ -165,8 +97,10 @@ export default function AppRouter() {
                 <Route path="categorias" element={<Categorias />} /> 
 
                 {/* 7. Acreditaciones */}
-                <Route path="creditos" element={<Creditos />} /> 
-                <Route path="reclamos" element={<Reclamos />} /> 
+                <Route path="creditos" element={<Creditos />} />
+                <Route path="creditos/revision/:id" element={<RevisionCredito />} />
+                <Route path="creditos/detalle/:id" element={<DetalleCredito />} />
+                <Route path="reclamos" element={<Reclamos />} />
 
                 {/* 8. Análisis */}
                 <Route path="reportes" element={<Reportes />} /> 
